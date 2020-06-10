3e53e9bc058f833aa0a17ec349fcdf3ea8dd4ba3
shivangtripathi <shivangtripathi2000@gmail.com>
2020-06-11 00:36:34 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React,{Component} from'react';
 import Cardlist from './Cardlist';
 import Searchbox from './Searchbox';
+import {robots} from './Robot'
 import ErrorBoundary from'./ErrorBoundary';
 class App extends Component{
     constructor(){
         super()
         this.state={
-            robots:[],
+            robots:robots,
             searchfield:''
         }
     }
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(users=>this.setState({robots:users}))
-    }
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response=>response.json())
+    //     .then(users=>this.setState({robots:users}))
+    // }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
