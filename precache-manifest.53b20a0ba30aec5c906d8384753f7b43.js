f686b1d47527eb05cd4e24d3736d4d9cafaba6d3
shivangtripathi <shivangtripathi2000@gmail.com>
2020-06-11 00:26:04 +0530

    Updates

---
+++ precache-manifest.53b20a0ba30aec5c906d8384753f7b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036e35f40b6ea5595effccdab5a156e6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f749f10905873ef19035",
+    "url": "/robofriends/static/css/main.247df7c7.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f749f10905873ef19035",
+    "url": "/robofriends/static/js/main.48bbdd24.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
